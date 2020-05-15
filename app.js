96793f47473b404858559124928861b8effb08aa
scrambleegg7 <scrambleegg7@gmail.com>
2020-05-15 11:04:28 +0900

    app static changed

--- app.js
+++ app.js
@@ -47,7 +47,10 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.static(path.join(__dirname, 'build')));
+
 
 app.use('/', indexRouter);
 app.use('/', listRouter);
